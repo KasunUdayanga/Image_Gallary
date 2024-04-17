badc157504ffd0282c87f56c71358a69579ab6b5
Kasun_Udayanga <udayangakasun696@gmail.com>
2024-04-17 22:30:47 +0530

    add project

---
+++ README.md
@@ -0,0 +1,2 @@
+# image-search-engine
+30 Days, 30 Projects. Day 15. Simple Image Search Engine using HTML, CSS, JavaScript
---
+++ script.js
@@ -0,0 +1,41 @@
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMore = document.getElementById("show-more");
+
+const accessKey = "MqkVBIRU2s4aZe1QYqXIgmVYHL0kj-n0Xr9-l9sO91c";
+let keyword = "";
+let page = 1;
+
+async function searchImages(){
+    keyword = searchBox.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const results = data.results;
+
+    results.map((result) =>{
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+
+        imageLink.appendChild(image);
+        searchResult.appendChild(imageLink);
+    });
+    showMore.style.display = "block"
+}
+
+searchForm.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    page = 1;
+    searchImages();
+});
+
+showMore.addEventListener("click",() => {
+    page++;
+    searchImages();
+});
